8cebc24f32c3e42cd02198dd83e0d19519c8dcbd
VillcaDenice <53920652+denicevillca@users.noreply.github.com>
2023-05-17 16:06:57 -0400

    subida 1

---
+++ database/seeders/CursoSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Curso;
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       /* $curso12 = new Curso();
+        $curso12->nombre = "vela" ;
+
+        $curso12->save();*/
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminte\Support\Facades\DB;
+use Illuminte\Support\Facades\Hash;
+use Illuminte\Support\Facades\Str;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Srt::random(10),
+            'email' => Srt::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
